4ef5ed999daa683d33b5b959a0a78c7100d7d68c
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-07-12 12:25:01 +0200

    Added conf for MySQL

---
+++ src/main/java/pl/milosz/shopbooks/ShopBooksApplication.java
@@ -0,0 +1,12 @@
+package pl.milosz.shopbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBooksApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/milosz/shopbooks/ShopBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milosz.shopbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
