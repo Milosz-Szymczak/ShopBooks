53dd132519304daaf612777328af8a2d2b04f8f5
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-02 15:04:55 +0200

    Add test BookRepository_FindAll_ReturnAllBooks

--- src/test/java/pl/milosz/shopbooks/repository/BookRepositoryTest.java
+++ src/test/java/pl/milosz/shopbooks/repository/BookRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import pl.milosz.shopbooks.model.Book;
 
+import java.util.List;
+
 @DataJpaTest
 public class BookRepositoryTest {
 
@@ -25,4 +27,26 @@ public class BookRepositoryTest {
         Assertions.assertThat(savedBook).isNotNull();
         Assertions.assertThat(savedBook.getId()).isGreaterThan(0);
     }
+
+    @Test
+    public void BookRepository_FindAll_ReturnAllBooks() {
+        //given
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        Book book1 = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        Book book2 = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        bookRepository.save(book);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+
+        //when
+        List<Book> allBooks = bookRepository.findAll();
+
+        //then
+        Assertions.assertThat(allBooks).isNotNull();
+        Assertions.assertThat(allBooks.size()).isEqualTo(3);
+    }
+
 }
