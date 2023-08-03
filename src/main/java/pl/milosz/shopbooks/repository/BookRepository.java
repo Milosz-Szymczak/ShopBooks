b4e3acfb88f320fa045649dfea1697090c2feef6
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-03 12:37:08 +0200

    Add new test BookRepository_findBooksByAuthor_ReturnBook

--- src/main/java/pl/milosz/shopbooks/repository/BookRepository.java
+++ src/main/java/pl/milosz/shopbooks/repository/BookRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.milosz.shopbooks.model.Book;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    List<Book> findBooksByAuthor(String author);
 }
--- src/test/java/pl/milosz/shopbooks/repository/BookRepositoryTest.java
+++ src/test/java/pl/milosz/shopbooks/repository/BookRepositoryTest.java
@@ -1,11 +1,9 @@
 package pl.milosz.shopbooks.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import pl.milosz.shopbooks.exception.ResourceNotFoundException;
 import pl.milosz.shopbooks.model.Book;
 
 import java.util.List;
@@ -66,9 +64,27 @@ public class BookRepositoryTest {
 
         //when
         Optional<Book> bookByID = bookRepository.findById(book2.getId());
-
         //then
         Assertions.assertThat(bookByID).isPresent();
       //System.out.println(BookByID.get());
+    }
+
+    @Test
+    public void BookRepository_findBooksByAuthor_ReturnBook() {
+        //given
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        Book book1 = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        Book book2 = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        bookRepository.save(book);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+
+        //when
+        List<Book> foundBooks = bookRepository.findBooksByAuthor("Martin Robert C.");
+        //then
+        Assertions.assertThat(foundBooks.size()).isGreaterThan(0);
     }
 }
