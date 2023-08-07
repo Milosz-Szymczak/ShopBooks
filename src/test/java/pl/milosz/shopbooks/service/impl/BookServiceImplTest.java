3707d370380f18a903dca177ac019316bc9bf80f
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-07 14:28:03 +0200

    Added test to check the saveBook method

--- src/test/java/pl/milosz/shopbooks/service/impl/BookServiceImplTest.java
+++ src/test/java/pl/milosz/shopbooks/service/impl/BookServiceImplTest.java
@@ -1,16 +1,38 @@
 package pl.milosz.shopbooks.service.impl;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.milosz.shopbooks.model.Book;
+import pl.milosz.shopbooks.repository.BookRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceImplTest {
 
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookServiceImpl bookServiceImpl;
+
     @Test
-    void saveBook() {
+    void BookService_saveBook_returnBook() {
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+
+        when(bookRepository.save(Mockito.any(Book.class))).thenReturn(book);
+        Book book1 = bookServiceImpl.saveBook(book);
+
+        Assertions.assertThat(book1).isNotNull();
+        verify(bookRepository).save(book);
     }
 
     @Test
