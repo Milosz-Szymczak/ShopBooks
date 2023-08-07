4c41d775335f11aa050cbe5b9b7e8e6220978429
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-07 15:49:02 +0200

    Added test BookService_getAllBooks_returnAllBooks

--- src/test/java/pl/milosz/shopbooks/service/impl/BookServiceImplTest.java
+++ src/test/java/pl/milosz/shopbooks/service/impl/BookServiceImplTest.java
@@ -10,6 +10,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import pl.milosz.shopbooks.model.Book;
 import pl.milosz.shopbooks.repository.BookRepository;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -36,7 +38,20 @@ class BookServiceImplTest {
     }
 
     @Test
-    void getAllBooks() {
+    void BookService_getAllBooks_returnAllBooks() {
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+        Book book2 = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+
+        List<Book> books = List.of(book, book2);
+
+        when(bookRepository.findAll()).thenReturn(books);
+
+        List<Book> allBooks = bookServiceImpl.getAllBooks();
+
+        Assertions.assertThat(allBooks).isNotNull();
+        Assertions.assertThat(allBooks.size()).isEqualTo(2);
     }
 
     @Test
