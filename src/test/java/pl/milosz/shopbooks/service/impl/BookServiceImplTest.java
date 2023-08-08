939bcde98e5c56e2a08b4ed66a4d13a44ef8c0df
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-08 13:48:01 +0200

    Added test findByID, updateBook, deleteBook

--- src/test/java/pl/milosz/shopbooks/service/impl/BookServiceImplTest.java
+++ src/test/java/pl/milosz/shopbooks/service/impl/BookServiceImplTest.java
@@ -1,18 +1,21 @@
 package pl.milosz.shopbooks.service.impl;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.milosz.shopbooks.exception.ResourceNotFoundException;
 import pl.milosz.shopbooks.model.Book;
 import pl.milosz.shopbooks.repository.BookRepository;
-
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -55,14 +58,51 @@ class BookServiceImplTest {
     }
 
     @Test
-    void findByID() {
+    void BookService_findByID_returnBookByID() {
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+
+        when(bookRepository.findById(1L)).thenReturn(Optional.ofNullable(book));
+
+        Book bookByID = bookServiceImpl.findByID(1L);
+
+        verify(bookRepository).findById(1L);
+        Assertions.assertThat(bookByID).isEqualTo(book);
     }
 
     @Test
-    void updateBook() {
+    void BookService_findByID_returnResourceNotFoundException() {
+        when(bookRepository.findById(2L)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> bookServiceImpl.findByID(2L));
     }
 
     @Test
-    void deleteBook() {
+    void BookService_updateBook_returnUpdatedBook() {
+        Book initialBook = Book.builder().name("Initial Title").author("Initial Author")
+                .kind("Initial Kind").releaseDate("2000-01-01").isbn(1234567890L).build();
+
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(initialBook));
+        when(bookRepository.save(book)).thenReturn(book);
+
+        Book updateBook = bookServiceImpl.updateBook(book, 1L);
+
+        Assertions.assertThat(updateBook).isEqualTo(book);
+        verify(bookRepository).save(book);
+    }
+
+    @Test
+    void BookService_deleteBook_returnVoid() {
+        Book book = Book.builder().name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+
+        assertAll(() -> bookServiceImpl.deleteBook(1L));
+
+        verify(bookRepository).findById(1L);
     }
 }
