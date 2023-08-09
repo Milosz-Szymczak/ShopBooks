e7c23477c9917ae017096b6aeb0727175ab52f53
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-09 15:03:20 +0200

    Added test GetAllBooks_CheckStatusAndJSONPatch_returnOK

--- src/test/java/pl/milosz/shopbooks/controller/BookControllerTest.java
+++ src/test/java/pl/milosz/shopbooks/controller/BookControllerTest.java
@@ -1,5 +1,6 @@
 package pl.milosz.shopbooks.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import pl.milosz.shopbooks.model.Book;
 import pl.milosz.shopbooks.service.BookService;
+
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -39,18 +42,36 @@ class BookControllerTest {
     }
 
     @Test
-    void getAllBooks() {
+    void GetAllBooks_CheckStatusAndJSONPatch_returnOK() throws Exception {
+        Book book = Book.builder().id(1L).name("Clean Code").author("Martin Robert C.")
+                .kind("Computer Science").releaseDate("2009-03-01").isbn(9780132350884L).build();
+
+        Book book2 = Book.builder().id(2L).name("a").author("a")
+                .kind("a").releaseDate("2001-03-01").isbn(32884L).build();
+
+        List<Book> allBooks = List.of(book,book2);
+
+        when(bookService.getAllBooks()).thenReturn(allBooks);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(allBooks)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value("Clean Code"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].id").value(2L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].name").value("a"));
     }
 
     @Test
-    void getBookById() {
+    void GetBookById_CheckStatusAndJSONPatch_returnCreated() {
     }
 
     @Test
-    void updateBookById() {
+    void UpdateBookById_CheckStatusAndJSONPatch_returnCreated() {
     }
 
     @Test
-    void deleteBookById() {
+    void DeleteBookById_CheckStatusAndJSONPatch_returnCreated() {
     }
 }
